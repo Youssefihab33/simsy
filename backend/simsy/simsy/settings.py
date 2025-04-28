79cc27d117efbd51dba1bad72b63e99ef83d1f33
Youssef Ihab <youssefihab33@gmail.com>
2025-04-28 19:30:37 +0300

    Created custom User model

--- backend/simsy/simsy/settings.py
+++ backend/simsy/simsy/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 ROOT_URLCONF = 'simsy.urls'
 
--- backend/simsy/users/models.py
+++ backend/simsy/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+class CustomUserManager(BaseUserManager): 
+    def create_user(self, username, password=None, **extra_fields ): 
+        if not username: 
+            raise ValueError('Username is a required field')
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,username, password=None, **extra_fields): 
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
+    
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+    nickname = models.CharField(max_length=30)
+    birthday = models.DateField(null=True, blank=True)
+    # profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    # nationality = models.CharField(max_length=100, blank=True, null=True)
+
+    # REQUIRED_FIELDS = ['email']  # Specify the required fields for user creation
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username
