4ebf511d33d5860df1a359497bc0ca73081ed812
Youssef Ihab <youssefihab33@gmail.com>
2025-07-28 02:43:41 +0300

    fixed logout bug

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocalStorage } from 'react-use';
 import { logout } from './APIs/AuthRequests';
 
+import LoadingSpinner from './snippets/LoadingSpinner';
+
 export default function Logout() {
+	const [token, setToken, removeToken] = useLocalStorage('auth_token');
 	const navigate = useNavigate();
 	useEffect(() => {
-		logout().finally(() => {
+		logout(token, removeToken).finally(() => {
 			navigate('/login/');
 		});
-	}, []);
+	}, [navigate, token, removeToken]);
+	return <LoadingSpinner />;
 }
